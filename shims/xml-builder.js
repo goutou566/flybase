ab5c7c391cb6314ea210cc1102e8e6f2811752d9
goutou566 <208224119+goutou566@users.noreply.github.com>
2026-08-24 18:19:20 +0000

    fix: use pure-JS XML parser shim for Workers runtime, pin account_id Co-authored-by: monkeycode-ai <monkeycode-ai@chaitin.com>

---
+++ shims/xml-builder.js
@@ -0,0 +1,3 @@
+export { parseXML } from '../node_modules/@aws-sdk/xml-builder/dist-es/xml-parser.js';
+export { XmlNode } from '../node_modules/@aws-sdk/xml-builder/dist-es/XmlNode.js';
+export { XmlText } from '../node_modules/@aws-sdk/xml-builder/dist-es/XmlText.js';
